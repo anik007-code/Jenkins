025377db1e73a6ce28083e6d3f01e6e8d9389b87
anik007-code <aniklpu01@gmail.com>
2024-04-28 22:01:26 +0600

    Jenkins configurations added

---
+++ gunicorn.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source venv/bin/activate
+
+# shellcheck disable=SC2164
+cd /var/lib/jenkins/workspace/django_cicd/app
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --no-input
+
+echo "Migrations done"
+
+# shellcheck disable=SC2164
+cd /var/lib/jenkins/workspace/django_cicd
+ls -l /var/lib/jenkins/workspace/django_cicd/
+
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "$USER"
+echo "$PWD"
+
+ls -l /var/lib/jenkins/workspace/django_cicd/
+
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+
+echo "Gunicorn has started."
+
+sudo systemctl enable gunicorn
+
+echo "Gunicorn has been enabled."
+
+sudo systemctl restart gunicorn
+
+
+sudo systemctl status gunicorn
